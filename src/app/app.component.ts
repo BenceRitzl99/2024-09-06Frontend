1c381181c0a30c960e5ffb24da093a1300edf4bc
Ritzl <benceritzl@gmail.com>
2024-09-06 11:32:16 +0200

    első frontend óra

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Cica';
+  nev = "Sándor"
+  tomb = ["Judit", "Eszter", 23, 45, true]
+  oszlopok = ["nev","kor", "Nem"]
+  emberek:any = [
+    {nev:"Attila", kor:50, Nem:"Nő"},
+    {nev:"Béla", kor:31, Nem:"Nő"},
+    {nev:"Dezső", kor:55, Nem:"Férfi"},
+    {nev:"András", kor:60, Nem:"Férfi"}
+    
+
+  ]
+  ujEmber:any ={}
+
+  hozzaAd(){
+    this.emberek.push(this.ujEmber)
+    this.ujEmber={}
+  }
+
+
+  torles(ember:any){
+    console.log(ember)
+    this.emberek = this.emberek.filter(
+      (e:any) => e!=ember
+    )
+  }
+
+  // felvetel(ujEmber:any){
+    
+  // }
 }
